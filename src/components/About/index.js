cc598543ecf8f97df587aa106b44861eb10cc8bd
terahje <terahjequinn@gmail.com>
2020-12-20 21:26:50 -0700

    updated about and css

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -8,17 +8,23 @@ function About() {
 			</div>
 			<div className="center">
 				<img
-					src={require("../../assets/images/profilePic.jpg")}
-					alt="picture of Terahje Gratkowski"
+					src={require("../../assets/images/pro-pic.jpg")}
+					alt="Terahje Gratkowski"
 					className="photo"
 				/>
 			</div>
 			<div>
 				<p>
-			{/* put here */}
+			Welcome and thank you for your time.  My name is Terahje Gratkowski and this webpage 
+			will be the embodiment of all the skills earned through the trials and successes that are a 
+			coding bootcamp.  
 				</p>
 				<p>
-		{/* put here */}
+			Growing up in Harrisburg, Pa, my life was more sports than acdemics.  While I love sports and long for
+			the compitiion, I've also had a deep love for technology and its various uses.  When I found coding it was
+			an immediate attraction, as I love the peeling back the layers of things we use day to day but fail to understand
+			how they work.  I was inspired by recent news clips as well and would love to continue to build on my knowledge base
+			with software development for robots.
 				</p>
 				<p>
 			{/* put here */}
